213046de645567cd343b421eca2a7a2e99556c35
Liliana Ortiz <lilianadcortiz@gmail.com>
2024-02-29 17:59:37 -0600

    added config.env and dotenv packages

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ const express = require('express');
 const app = express();
 
 
+app.get('/', (req, res) => {
+  res.send("WELCOME TO MY PAGE!")
+});
+
+
+app.get('/employees', (req,res) => {
+  res.send('Employees');
+})
+
 app.listen(3000);
