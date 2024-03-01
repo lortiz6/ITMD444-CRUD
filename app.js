8d77d7e4388fc2a135fffbb7df364418ea31ad54
Liliana Ortiz <lilianadcortiz@gmail.com>
2024-02-29 21:42:56 -0600

    closed a parenthesis in app.js

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-
-
 const app = express();
-app.use(bodyParser.json)
+app.use(bodyParser.json());
 
 const dotenv = require('dotenv');
-
 const connectDB = require('./config/db');
 
-//Load Config
-dotenv.config({path: './config/config.env'});
+// Load Config
+dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
-//Routes
+// Routes
 app.use('/', require('./routes/index'));
 
 app.listen(3000);
